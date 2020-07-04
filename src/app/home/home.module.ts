83e1e94d05f19417be9597fb0dab861746c522fd
bibhu-padhy <bibhupadhy140@gmail.com>
2020-07-04 21:39:33 +0530

    home border and nav bar UI

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { InventoryItemComponent } from './inventory-item/inventory-item.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, InventoryItemComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
